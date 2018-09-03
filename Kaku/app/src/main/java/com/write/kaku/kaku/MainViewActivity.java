b2cfe5e6a6e21451b35f108d2199faf82030ca95
guswls6212 <guswls6212@gmail.com>
2018-09-03 21:43:21 +0900

    Revert "툴바 검색버튼 표시"
    This reverts commit 426c2eb12bbabbcf8c8ed28103815404f167782c.

--- Kaku/app/src/main/java/com/write/kaku/kaku/MainViewActivity.java
+++ Kaku/app/src/main/java/com/write/kaku/kaku/MainViewActivity.java
@@ -107,12 +107,12 @@ public class MainViewActivity extends AppCompatActivity
         }
     }
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
-    }
+    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
